e0db80206aadeac060ff9e985cd4c34c9355510b
Cpapa97 <cpapa97@gmail.com>
2018-12-08 19:13:24 -0600

    Removed version requirement, hasn't been tested for anything other than 3.6 though

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
-# Is this necessary?
-# if sys.version_info < (3, 6):
-#     sys.exit('LabelOps is only guaranteed to be supported on Python 3.6 or higher')
-
 here = path.abspath(path.dirname(__file__))
 
 long_description = "A package to handle specific operations on neuronal meshes with labeled data, including compressing and decompressing those labels."
@@ -26,7 +22,7 @@ setup(
     author='Christos Papadopoulos',
     author_email='cpapa97@gmail.com',
     license="GNU LGPL",
-    url='https://github.com/Cpapa97/LabelOps',
+    url='https://github.com/Cajal/LabelOps',
     keywords='functional connectomics',
     packages=['labelops'],
     install_requires=requirements,
